f981fcf84ab587d63d4bbb78608e4866f2e37630
weblogger117-art <weblogger117@gmail.com>
2026-06-09 10:41:40 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,25 @@
 
-const CACHE='gothic-schlossknacker-v64';
-const ASSETS=[
- './',
- './index.html',
- './manifest.json',
- './icon-192.png',
- './icon-512.png',
- './Splashscreen.png'
+const CACHE_NAME = "gothic-schlossknacker-v643";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./Splashscreen.png"
 ];
-self.addEventListener('install',e=>{
- e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
-self.addEventListener('fetch',e=>{
- e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
